acf07eafd21f2aa7be3cf6c5d820b93ee54dd87b
Marcel Melo <marcel.mdr@gmail.com>
2020-07-07 15:10:41 -0400

    Created custom HandleError

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,23 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -50,6 +62,20 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        // Only for the API
+        if( $request->is('api/*')) {
+            return $this->handleApiExceptions( $request, $exception);
+        }
         return parent::render($request, $exception);
     }
+
+    private function handleApiExceptions( $request, Throwable $exception) {
+
+        if ( $exception instanceof NotFoundHttpException) {
+            return $this->errorResponse( "The Specified URL or Route doesn't exist!", [],404);
+        }
+
+        return $this->errorResponse( $exception->getMessage(), [], 404);
+    }
+
 }
